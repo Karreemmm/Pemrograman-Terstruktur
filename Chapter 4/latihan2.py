f59b7361d62c5e66fa19d621401f3620541f4f67
Nabil Makarim <89756149+Karreemmm@users.noreply.github.com>
2021-10-10 22:14:54 +0700

    Chapter 4

---
+++ Chapter 4/latihan2.py
@@ -0,0 +1,8 @@
+jarakKota = 795
+konsumsiBensin = 1/12
+
+# Hitung penggunaan bensin
+totalBensin = jarakKota * konsumsiBensin
+
+# Menampilkan hasil
+print("Bensin yang diperlukan untuk perjalanan dari kota A menuju kota C adalah", totalBensin, "Liter")
