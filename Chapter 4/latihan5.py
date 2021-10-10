d609fbce1b359f759cdcda4474bfc3bb0caf7623
Nabil Makarim <89756149+Karreemmm@users.noreply.github.com>
2021-10-10 22:19:34 +0700

    Chapter 4

---
+++ Chapter 4/latihan5.py
@@ -0,0 +1,7 @@
+# Input jumlah
+jmlhLaki = int(input("Jumlah Mahasiswa laki-laki: "))
+jmlhPerempuan = int(input("Jumlah Mahasiswa perempuan: "))
+
+# Menampilkan jumlah
+print("Laki-laki  :", "*" * (jmlhLaki // 3),jmlhLaki)
+print("Perempuan  :", "*" * (jmlhPerempuan // 3),jmlhPerempuan)
