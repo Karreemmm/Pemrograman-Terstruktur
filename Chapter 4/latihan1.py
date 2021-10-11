3249a6e2d02d3f76324f6fadedb5160d5540d151
Nabil Makarim <89756149+Karreemmm@users.noreply.github.com>
2021-10-11 12:03:48 +0700

    Chapter 4

--- Chapter 4/latihan1.py
+++ Chapter 4/latihan1.py
@@ -1,1 +1,23 @@
+# Waktu rental
+jamMasuk = 6
+menitMasuk = 0
+jamKeluar = 23
+menitKeluar = 50
 
+# Biaya rental 12 jam pertama
+biaya12Jam = 200000
+ 
+# Biaya rental per 1 jam
+biaya1Jam = 10000
+
+# Selisih waktu rental
+selisihJam = jamKeluar - jamMasuk
+selisihMenit = menitKeluar - menitMasuk
+
+# Total biaya rental
+totalBiaya = biaya12Jam + biaya1Jam * (selisihJam -12)
+totalBiaya = totalBiaya + biaya1Jam * (selisihMenit / 60)
+totalBiaya = round(totalBiaya)
+
+print ('Total waktu rental adalah', selisihJam, 'Jam', selisihMenit, 'Menit' )
+print ('Total biaya rental adalah Rp', totalBiaya)
