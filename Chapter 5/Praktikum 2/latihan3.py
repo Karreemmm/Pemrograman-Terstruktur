dc295d58de1210daf113b0bca73ef19943215d65
Nabil Makarim <89756149+Karreemmm@users.noreply.github.com>
2021-10-17 21:17:17 +0700

    Chapter 5

---
+++ Chapter 5/Praktikum 2/latihan3.py
@@ -0,0 +1,10 @@
+banyak = 0
+jumlah = 0
+i = 1
+while(i < 100):
+    print(i)
+    i += 2
+    banyak += 1
+    jumlah += i
+print('Banyaknya bilangan ganjil : ' , banyak)
+print('Jumlah seluruh bilangan   : ' , jumlah)
