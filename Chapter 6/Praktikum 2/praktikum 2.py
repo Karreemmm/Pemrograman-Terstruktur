1a9aee7402ecc96157abf0a71632a1175d5cb80f
Nabil Makarim <89756149+Karreemmm@users.noreply.github.com>
2021-10-31 23:30:42 +0700

    Chapter 6

---
+++ Chapter 6/Praktikum 2/praktikum 2.py
@@ -0,0 +1,24 @@
+print('\n')
+def luasSegitiga(a, t):
+    luas = a * t / 2
+    return luas
+
+alas = 10
+tinggi = 20
+
+alas = 15
+tinggi = 45
+print('Luas segitiga dg alas', alas, 'dan tinggi', tinggi, 'adalah', luasSegitiga(alas, tinggi) + luasSegitiga(alas, tinggi))
+
+print('\n')
+def luasSegitiga2(a, t):
+    luas = a * t / 2
+    print('Luas segitiga dg alas', alas, 'dan tinggi', tinggi, 'adalah', luas)
+    
+
+alas = 10
+tinggi = 20
+luasSegitiga2(10, 20)
+luasSegitiga2(15, 45)
+luasSegitiga2(10+15, 20+45)
+
