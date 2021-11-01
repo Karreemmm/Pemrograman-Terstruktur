072ce547c6c28139540dc2645c30e86dfc272f48
Nabil Makarim <89756149+Karreemmm@users.noreply.github.com>
2021-11-01 16:05:50 +0700

    Create latihan5.py

---
+++ Chapter 6/latihan5.py
@@ -0,0 +1,13 @@
+from statistik import *
+
+print('a.Data : 5, 10, 4, 9, 30, 16, 2, 11 ')
+print('Total     =',sum(5, 10, 4, 9, 30, 16, 2, 11))
+print('Rata-rata =',avg(5, 10, 4, 9, 30, 16, 2, 11))
+print('Maksimum  =',max(5, 10, 4, 9, 30, 16, 2, 11))
+print('Minimum   =',min(5, 10, 4, 9, 30, 16, 2, 11))
+print('\n')
+print('b.Data : 81, 98, 12, 83, 45, 77, 69, 30, 56 ')
+print('Total     =',sum(81, 98, 12, 83, 45, 77, 69, 30, 56))
+print('Rata-rata =',avg(81, 98, 12, 83, 45, 77, 69, 30, 56))
+print('Maksimum  =',max(81, 98, 12, 83, 45, 77, 69, 30, 56))
+print('Minimum   =',min(81, 98, 12, 83, 45, 77, 69, 30, 56))
