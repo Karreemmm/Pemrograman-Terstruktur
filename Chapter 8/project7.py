5b9acc10ebd352b9a91536f6dc3632ccff38d5d0
Nabil Makarim <89756149+Karreemmm@users.noreply.github.com>
2021-11-26 15:19:47 +0700

    Chapter 8

---
+++ Chapter 8/project7.py
@@ -0,0 +1,12 @@
+buah = {'apel' : 5000, 'jeruk' : 8500, 'mangga' : 7800, 'duku' : 6500}
+
+def mahal():
+    print(buah)
+    key = list(buah.keys())
+    value = tuple(buah.values())
+    termahal = max(value)
+    index = value.index(termahal)
+    
+    print('Buah Termahal :',key[index])
+
+mahal()
