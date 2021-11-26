2f22ac49dcb57794c516b829c169d520481470ce
Nabil Makarim <89756149+Karreemmm@users.noreply.github.com>
2021-11-26 15:19:19 +0700

    Chapter 8

---
+++ Chapter 8/project6.py
@@ -0,0 +1,8 @@
+myData = ['apel','rambutan','jeruk']
+print('myData :', (myData))
+
+def sortStringByChar(myData) :
+    myData.sort(reverse = True)
+    return myData
+    
+print('myData :', sortStringByChar(myData))
