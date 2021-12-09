1f380d2191f1282570f92bb82fc0d656b20f541b
Nabil Makarim <89756149+Karreemmm@users.noreply.github.com>
2021-12-09 19:33:23 +0700

    Chapter 11

---
+++ Chapter 11/project1.py
@@ -0,0 +1,11 @@
+from datetime import *
+
+def diffDate (x) :
+    selisih = abs(x - y)
+    hasil = selisih.days
+    
+    return hasil
+
+y = datetime.date(datetime.now())
+x = date(2021,12,31)
+print('Selisih hari antara tanggal', y, 'dengan', x, 'adalah', diffDate(x), 'hari.')
